2bf12289f5b0ee30b7966381595559fd61ce83fb
qbv2001 <huutinh999999@gmail.com>
2023-03-17 13:09:04 +0700

    dang nhap va doi mat khau

--- QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/Properties/Resources.Designer.cs
+++ QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace QLNS_TinhVanSoftWare.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bg {
+            get {
+                object obj = ResourceManager.GetObject("bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bg1 {
+            get {
+                object obj = ResourceManager.GetObject("bg1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Close_24_1px {
             get {
                 object obj = ResourceManager.GetObject("Close_24_1px", resourceCulture);
