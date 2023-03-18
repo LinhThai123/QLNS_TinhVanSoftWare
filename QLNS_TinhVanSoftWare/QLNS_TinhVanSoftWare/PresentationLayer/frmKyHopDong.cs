6c9ba9a19a4de8a59819eb8f0e4f2ee4b7161391
Linhthai123 <Linhthai130100@gmail.com>
2023-03-18 22:08:16 +0700

    merge dtlong

---
+++ QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/PresentationLayer/frmKyHopDong.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNS_TinhVanSoftWare.PresentationLayer
+{
+    public partial class frmKyHopDong : Form
+    {
+        public frmKyHopDong()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //Mã nhân viên, Tên nhân viên, Ngày sinh, Giới tính, Số CCCD, Địa chỉ, SĐT, Email, Ngày vào làm, Mã chức vụ, Tên chức vụ, Mã phòng ban, Tên phòng ban, Lương, Tình trạng)
+
+
+        //Mã hợp đồng, Ngày ký, Thời hạn, Mã nhân viên, Tên nhân viên, Mã chức vụ, Tên chức vụ, Mã phòng ban, Tên phòng ban, Lương
+    }
+}
