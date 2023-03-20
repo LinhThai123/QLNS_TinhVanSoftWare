2c177a3b9468831809f1330cbe40e57d09ecd7b1
diuthanh520 <126053865+diuthanh520@users.noreply.github.com>
2023-03-20 21:06:04 +0700

    Form cham cong

---
+++ QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/DataAccessLayer/NhanVienDAL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNS_TinhVanSoftWare.DataAccessLayer
+{
+    class NhanVienDAL
+    {
+        private SqlConnection connection;
+
+        public NhanVienDAL()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+        }
+        // Phương thức mở kết nối
+        public void OpenConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        // Phương thức đóng kết nối
+        public void CloseConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+    /*    // Phương thức thêm tài khoản
+        public bool ThemTaiKhoan(string maNV, string taiKhoan, string matKhau, string quyen)
+        {
+            OpenConnection();
+            SqlCommand cmd = new SqlCommand("sp_ThemTaiKhoan", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@maNV", maNV);
+            cmd.Parameters.AddWithValue("@taiKhoan", taiKhoan);
+            cmd.Parameters.AddWithValue("@matKhau", matKhau);
+            cmd.Parameters.AddWithValue("@quyen", quyen);
+
+            int result = cmd.ExecuteNonQuery();
+            CloseConnection();
+            return result > 0;
+        }
+
+
+        // Phương thức sửa tài khoản
+        public bool SuaTaiKhoan(string maNV, string taiKhoan, string matKhau, string quyen)
+        {
+            OpenConnection();
+            SqlCommand cmd = new SqlCommand("sp_SuaTaiKhoan", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@maNV", maNV);
+            cmd.Parameters.AddWithValue("@taiKhoan", taiKhoan);
+            cmd.Parameters.AddWithValue("@matKhau", matKhau);
+            cmd.Parameters.AddWithValue("@quyen", quyen);
+
+            int result = cmd.ExecuteNonQuery();
+            CloseConnection();
+            return result > 0;
+        }
+        // Phương thức xóa tài khoản
+        public bool XoaTaiKhoan(string maNV)
+        {
+            OpenConnection();
+            SqlCommand cmd = new SqlCommand("sp_XoaTaiKhoan", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@maNV", maNV);
+
+            int result = cmd.ExecuteNonQuery();
+            CloseConnection();
+            return result > 0;
+        }
+*/
+
+
+
+  /*      // kiểm tra đăng nhập
+        public bool KiemTraDangNhap(string taiKhoan, string matKhau)
+        {
+            OpenConnection();
+            SqlCommand cmd = new SqlCommand("sp_KiemTraDangNhap", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@taiKhoan", taiKhoan);
+            cmd.Parameters.AddWithValue("@matKhau", matKhau);
+
+            int result = (int)cmd.ExecuteScalar();
+            CloseConnection();
+            return result > 0;
+        }*/
+
+        // đổ dữ liệu ra combobox
+        public DataTable LayDanhSachMaNhanVien()
+        {
+            OpenConnection();
+            DataTable dt = new DataTable();
+            string query = "SELECT PK_sMaNV,sTenNV FROM tbl_NhanVien;";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.Fill(dt);
+            CloseConnection();
+            return dt;
+        }
+
+    }
+}
--- QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/PresentationLayer/frmChamCong.cs
+++ QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/PresentationLayer/frmChamCong.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using QLNS_TinhVanSoftWare.BusinessLogicLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,112 @@ namespace QLNS_TinhVanSoftWare.PresentationLayer
         public frmChamCong()
         {
             InitializeComponent();
+        }
+        public void HienThiMaNhanVien()
+        {
+            NhanVienBBL nhanVienBBL = new NhanVienBBL();
+            DataTable dt = nhanVienBBL.LayDanhSachMaNhanVien();
+            cmbMaNv.DataSource = dt;
+            cmbMaNv.DisplayMember = "PK_sMaNV";
+            cmbMaNv.ValueMember = "PK_sMaNV";
+
+            /*cmbMaNv.DisplayMember = "PK_sMaNV"; // hiển thị giá trị của cột "idNhanVien"
+            cmbMaNv.ValueMember = "PK_sMaNV"; // giá trị của mỗi item trong combobox cũng là giá trị của cột "idNhanVien"
+            cmbMaNv.FormatString = "{0} - {1}"; // định dạng hiển thị của mỗi item là "idNhanVien - tenNhanVien"
+            cmbMaNv.DataSource = dt; // myDataTable là bảng dữ liệu chứa thông tin nhân viên
+*/
+
+        }
+
+        private void cmbMaNv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmChamCong_Load(object sender, EventArgs e)
+        {
+            HienThiMaNhanVien();
+            LoadDataIntoDataGridView();
+            CustomizeColumnHeaders();
+           
+
+        }
+        private void CustomizeColumnHeaders()
+        {
+            /*// Thay đổi tên cột dựa trên tên cột trong cơ sở dữ liệu
+            dataGridView1.Columns["ColumnNameFromDatabase"].HeaderText = "Tên cột mới";*/
+
+            // Hoặc bạn có thể thay đổi tên cột dựa trên chỉ mục của cột
+            dgvChamCong.Columns[0].HeaderText = "Mã chấm công";
+            dgvChamCong.Columns[1].HeaderText = "Mã NV";
+            dgvChamCong.Columns[2].HeaderText = "Ngày chấm công";
+            dgvChamCong.Columns[3].HeaderText = "Giờ vào";
+            dgvChamCong.Columns[4].HeaderText = "Giờ ra";
+            dgvChamCong.Columns[5].HeaderText = "Số giờ làm";
+            dgvChamCong.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+        }
+        private void LoadDataIntoDataGridView()
+        {
+            // Thay đổi chuỗi kết nối này để phù hợp với cấu hình SQL Server của bạn
+            string connectionString = @"Data Source=DESKTOP-96D4EUK\ADMIN;Initial Catalog=QuanLyNhanVien;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Thay đổi truy vấn này để lấy dữ liệu từ bảng bạn muốn
+                string query = "SELECT * FROM tbl_Chamcong";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                // Gán dữ liệu từ DataTable vào DataGridView
+                dgvChamCong.DataSource = dataTable;
+            }
+        }
+
+        private void dtpNgayChamCong_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = dtpNgayChamCong.Value;
+            
+        }
+
+        private void dgvChamCong_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string maNhanVien = cmbMaNv.Text.Trim();
+            string ngayChamCong = dtpNgayChamCong.Value.ToString("yyyy-MM-dd");
+
+
+            string connectionString = @"Data Source=DESKTOP-96D4EUK\ADMIN;Initial Catalog=QuanLyNhanVien;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @"SELECT * FROM tbl_ChamCong WHERE
+                            (FK_sMaNV = @MaNhanVien )
+                            AND (CONVERT(date, dNgaychamcong) = @NgayChamCong)";
+
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaNhanVien", maNhanVien);
+                command.Parameters.AddWithValue("@NgayChamCong", dtpNgayChamCong.Value);
+
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                // Gán dữ liệu từ DataTable vào DataGridView
+                dgvChamCong.DataSource = dataTable;
+            }
         }
     }
 }
