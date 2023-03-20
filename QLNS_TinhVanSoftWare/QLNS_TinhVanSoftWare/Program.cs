bdc64ad81ea2b9dcedb2acb68146f1d9c241ebdc
KyThuat88 <KyThuat88@DESKTOP-KLPOHE4>
2023-03-20 22:25:33 +0700

    code frmKhenThuongKyLuat

---
+++ QLNS_TinhVanSoftWare/QLNS_TinhVanSoftWare/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNS_TinhVanSoftWare
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            /// <summary>
+            /// The main entry point for the application.
+            /// </summary>
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PresentationLayer.frmTrangChu());
+        }
+    }
+}
